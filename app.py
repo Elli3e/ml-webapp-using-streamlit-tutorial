e394c248b36c0ab5206f0f77e15ecb50566772c2
Ellie Babaie <e_babaie@yahoo.com>
2026-08-25 15:55:53 -0700

    streamlit_completed

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import tensorflow as tf
+import yfinance as yf
+from pickle import load
+import numpy as np
+
+st.title("Stock Price Prediction")
+
+with open ("scale_min_max.sav", "rb") as f:
+ scale = load(f) 
+
+prediction_model = tf.keras.models.load_model("lstm_model.keras")
+
+ticker = st.text_input("Enter stock ticker", "BABA")
+
+if st.button("Get Stock Data"):
+    
+    data = yf.download(ticker, period="1y")
+    close_data = data[["Close"]]
+    scaled_stock = scale.transform(close_data)
+    x_input= scaled_stock[-60:,0]
+    x_input = np.array(x_input)
+    x_input=x_input.reshape(1, 60, 1)
+    pred= prediction_model.predict(x_input)
+    prediction = scale.inverse_transform(pred)
+
+    st.write("prediction price :", prediction[0][0]) 
