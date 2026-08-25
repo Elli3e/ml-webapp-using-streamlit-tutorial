799a08f962e641042b545ef2764d00d0eb380768
Ellie Babaie <e_babaie@yahoo.com>
2026-08-25 16:26:08 -0700

    streamlit_completed

--- app.py
+++ app.py
@@ -14,14 +14,25 @@ prediction_model = tf.keras.models.load_model("lstm_model.keras")
 ticker = st.text_input("Enter stock ticker", "BABA")
 
 if st.button("Get Stock Data"):
-    
+
     data = yf.download(ticker, period="1y")
+
+    st.write("Downloaded data shape:", data.shape)
+    st.write(data.tail())
+
     close_data = data[["Close"]]
+
+    st.write("Close data shape:", close_data.shape)
+    st.write(close_data.tail())
+
     scaled_stock = scale.transform(close_data)
-    x_input= scaled_stock[-60:,0]
+
+    x_input = scaled_stock[-60:, 0]
     x_input = np.array(x_input)
-    x_input=x_input.reshape(1, 60, 1)
-    pred= prediction_model.predict(x_input)
+    x_input = x_input.reshape(1, 60, 1)
+
+    pred = prediction_model.predict(x_input)
+
     prediction = scale.inverse_transform(pred)
 
-    st.write("prediction price :", prediction[0][0]) 
+    st.write("prediction price:", prediction[0][0])
